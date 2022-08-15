37d69a90228557b2897069da9152babf6daa1b92
tuba kaya <tasaf1987@gmail.com>
2022-08-15 11:59:55 +0300

    v2

--- src/Day1Git1.java
+++ src/Day1Git1.java
@@ -1,5 +1,7 @@
 public class Day1Git1 {
     public static void main(String[] args) {
         System.out.println("hello git 1.deneme ");
+        System.out.println("hello git 2.deneme ");
+        System.out.println("hello git 3.deneme ");
     }
 }
